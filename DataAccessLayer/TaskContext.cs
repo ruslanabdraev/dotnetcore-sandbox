807b61af4d006d9b4a8f47e03e4284aabf52ae6a
ruslan.abdraev <ruslan.abdraev@havi.com>
2020-02-16 18:47:57 +0300

    Added DatabaseContext

---
+++ DataAccessLayer/TaskContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace DataAccessLayer
+{
+    public class TaskContext : DbContext
+    {
+        public TaskContext(DbContextOptions options) : base(options)
+        { }
+
+        public virtual DbSet<Task> Tasks { get; set; }
+    }
+}
--- WebApi/Startup.cs
+++ WebApi/Startup.cs
@@ -3,10 +3,12 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using BusinessLayer;
+using DataAccessLayer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -27,7 +29,8 @@ namespace WebApi
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
-            services.AddBusinessLayer();
+            services.AddDabaseContext(Configuration)
+                .AddBusinessLayer();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -55,6 +58,18 @@ namespace WebApi
 
     public static class Extensions
     {
+        public static IServiceCollection AddDabaseContext(this IServiceCollection services, IConfiguration config)
+        {
+            var connection = config.GetConnectionString("DatabaseContext");
+
+            services.AddDbContext<TaskContext>((optionsBuilder) =>
+            {
+                optionsBuilder.UseSqlServer(connection);
+            });
+
+            return services;
+        }
+
         public static IServiceCollection AddBusinessLayer(this IServiceCollection services)
         {
             return services.AddScoped<ITaskService, TaskService>();
