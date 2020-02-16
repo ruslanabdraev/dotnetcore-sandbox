3dd69746ad540c2041d83c12c5139d05db07b728
ruslan.abdraev <ruslan.abdraev@havi.com>
2020-02-16 19:12:15 +0300

    Added AutoMapper

---
+++ BusinessLayer/AppMappingProfile.cs
@@ -0,0 +1,18 @@
+﻿using AutoMapper;
+using Entities = DataAccessLayer;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BusinessLayer
+{
+    public class AppMappingProfile : Profile
+    {
+        public AppMappingProfile()
+        {
+            CreateMap<Contract.Task, Entities.Task>()
+                .ForMember(c => c.TaskId, o => o.MapFrom(o => o.Id))
+                .ForMember(c => c.TaskName, o => o.MapFrom(o => o.Name));
+        }
+    }
+}
